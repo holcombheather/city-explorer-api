ea361db5b4b99fc7b23b7651de703395020d01cf
Heather Holcomb <holcombheather@gmail.com>
2023-04-18 10:54:27 -0700

    add const app

--- server.js
+++ server.js
@@ -7,3 +7,5 @@ const express = require('express');
 require('dotenv').config();
 const cors = require('cors');
 
+// ** app === server - Need to call Express to create the server
+const app = express();
