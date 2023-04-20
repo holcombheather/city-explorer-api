a81ce4441aadf4f81c892bd343c7d57529c687bf
Heather Holcomb <holcombheather@gmail.com>
2023-04-20 08:51:16 -0700

    Updated from class lecture demo

--- server.js
+++ server.js
@@ -1,15 +1,13 @@
 'use strict';
 
-// REQUIRE - import for backend
 const express = require('express');
 require('dotenv').config();
 const cors = require('cors');
 let weatherData = require('./data/weather.json');
+// const axios = require('axios');
 
-// EXPRESS - need to call Express to create the server (app === server)
 const app = express();
 
-// MIDDLEWARE - allow anyone to hit our server
 app.use(cors());
 
 const PORT = process.env.PORT || 3002;
@@ -17,63 +15,87 @@ const PORT = process.env.PORT || 3002;
 app.listen(PORT, () => console.log(`Woohoos! we are up on port ${PORT}`));
 
 
-
-// ENDPOINTS
-//         (1st arg - endpoint url, 2nd arg - callback which will execute when that endpoint is hit)
-//                => 2 parameters: request, response
 app.get('/', (request, response)=>{
   response.status(200).send('Welcome to my City Explorer server!');
 });
 
-// Weather Endpoint
 app.get('/weather', (request, response, next)=>{
+  console.log('Weather endpoint hit');
+  console.log('All weather data: ', weatherData);
 
   try {
     // let lat = request.query.lat;
     // let lon = request.query.lon;
     let searchQuery = request.query.searchQuery;
 
+
+    // TODO: ERROR MESSAGE
+
     let foundCityWeather = weatherData.find(city => city.city_name === searchQuery);
-    let forecast = new Forecast();
-    let forecastArr = forecast.getForecast(foundCityWeather);
 
-    console.log(request.query);
+    console.log('foundCityWeather:', foundCityWeather);
 
-    response.status(200).send(forecastArr);
+    let forecasts = foundCityWeather.data.map(weatherData => new Forecast(weatherData));
+
+
+    response.status(200).send(forecasts);
   } catch (error) {
     console.log(error.message);
     next(error);
   }
 });
 
-// Forecast Class - Using a class with an object constructor to create a simpler object
 class Forecast {
-  constructor(){}
-  getForecast(forecastArr) {
-    let simpleForecastArr = [];
-    for (let i = 0; i < forecastArr.data.length; i++) {
-      let forecastObj = {
-        date: forecastArr.data[i].valid_date,
-        description: `Low of ${forecastArr.data[i].low_temp} degrees Celsius, high of ${forecastArr.data[i].high_temp} degrees Celsius with ${forecastArr.data[i].weather.description}`
-      };
-      simpleForecastArr.push(forecastObj);
-    }
-    return simpleForecastArr;
+  constructor(forecastObj){
+    this.date = forecastObj.valid_date;
+    this.description = forecastObj.weather.description;
   }
 }
 
-let forecast = new Forecast();
-let forecastArr = forecast.getForecast(weatherData[0]);
-console.log(forecastArr);
+// getForecast(forecastObj) {
+//   let formattedForecast = [];
+//   for (let i = 0; i < forecastObj.length; i++) {
+//     let forecastArr = {
+//       date: forecastObj[i].valid_date,
+//       description: `Low of ${forecastObj.data[i].low_temp} degrees Celsius, high of ${forecastObj.data[i].high_temp} degrees Celsius with ${forecastObj.data[i].weather.description}`
+//     };
+//     formattedForecast.push(forecastArr);
+//   }
+//   return formattedForecast;
+// }
+
+// TODO: BUILD AN ENDPOINT THAT WILL CALL OUT TO AN API
+// app.get('/photos', getPhotos);
+
+// async function getPhotos (request, response, next) {
+//   try {
+//     let myLocalCity = request.query.city;
+
+//     let url=`https://api.unsplash.com/search/photos?client_id=${process.env.UNSPLASH_API_KEY}&query=${myLocalCity}`;
+
+//     let photosFromAxios = await axios.get(url);
+
+//     let dataToSend = photosFromAxios.data.results.map(obj => new Photo(obj));
+
+//     response.status(200).send(dataToSend);
+//   } catch (error) {
+//     next(error);
+//   }
+// }
+
+// class Photo {
+//   constructor(picObj){
+//     this.src = picObj.urls.regular;
+//     this.alt = picObj.alt_description;
+//     this.username = picObj.user.name;
+//   }
+// }
 
 
-
-// CATCH ALL ENDPOINT - should be the very last endpoint defined
 app.get('*', (request,response) =>{
   response.status(404).send('This page does not exist');
 });
 
-// ERROR HANDLING - Plug and play code from Express Docs
 app.use((error, request, response, next) => {
   console.log(error.message);
   response.status(500).send(error.message);
